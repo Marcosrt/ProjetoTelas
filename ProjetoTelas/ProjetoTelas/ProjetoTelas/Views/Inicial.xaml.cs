ce306c7753dce2d11b899376dec5bdd98269336f
Marcos Roberto Tetzlaff <marcosrt22@hotmail.com>
2017-09-06 22:23:50 -0300

    Subindo o ProjetoTelas 1

---
+++ ProjetoTelas/ProjetoTelas/ProjetoTelas/Views/Inicial.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjetoTelas.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicial : ContentPage
+    {
+        public Inicial()
+        {
+            InitializeComponent();
+        }
+
+
+        public void botaoConsulta_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AgendarConsulta());
+        }
+
+        private void botaoLivro_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CadastrarLivro());
+        }
+
+        private void botaoTime_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AdicionarTime());
+        }
+
+        private void botaoInteresse_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegistrarInteresse());
+        }
+    }
+}
